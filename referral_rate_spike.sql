385f3e9ee0be8191ad33de334681bf0896a64545
Innocent <93262439+Ti-innocent@users.noreply.github.com>
2026-04-26 16:00:51 +0200

    referral_rate_spike.sql

--- referral_rate_spike.sql
+++ referral_rate_spike.sql
@@ -2,10 +2,10 @@ WITH monthly AS (
     SELECT
         month,
         doctor_referral_rate,
-        LAG(doctor_referral_rate) OVER (ORDER BY month) AS prev_rate
+        LAG(doctor_referral_rate) OVER (ORDER BY month) AS previous_rate
     FROM {{ ref('mart_referral_rate_monthly') }}
 )
 SELECT *
 FROM monthly
-WHERE prev_rate IS NOT NULL
-  AND doctor_referral_rate > prev_rate * 2
+WHERE previous_rate IS NOT NULL
+  AND doctor_referral_rate > previous_rate * 2
